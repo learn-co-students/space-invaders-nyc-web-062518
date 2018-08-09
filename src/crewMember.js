4557e8e7debc1068501719c8a6c04a1d5ce7c7e9
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-08-09 15:16:10 -0400

    classes created

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,20 @@
-class CrewMember {}
+class CrewMember {
+
+  constructor(position, currentShip) {
+    this.position = position;
+    // this.currentShip = currentShip;
+    // this.currentShip = "Looking for a Rig"
+  }
+
+  engageWarpDrive(currentShip) {
+    currentShip === "Looking for a Rig" ? return "had no effect" : this.currentShip.warpDrive = "engaged"
+  }
+
+  setsInvisibility() {
+
+  }
+
+  chargePhasers() {
+
+  }
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,13 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name;
+    this.crew = [];
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = "disengaged"
+    this.docked = true;
+    this.phasersCharge = "uncharged"
+    crew.length > 0 ? this.docked = false : this.docked = true
+  }
+}
