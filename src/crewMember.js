744a410091354ae0288c9c324c15c76dca22d654
Daniel Ran <danielr1735@gmail.com>
2018-08-09 15:43:40 -0400

    bon voyage

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,45 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position) {
+
+    this.position = position
+    // if (shipId) {
+    //   this.shipId = shipId
+    // }
+  }
+
+  currentShip() {
+    if (this.shipId) {
+      console.log("ship ship")
+      return spaceships.find(function (spaceship) {
+        return spaceship.id === this.shipId
+      })
+    } else {
+      console.log('looking for a rig')
+      return 'Looking for a Rig'
+    }
+  }
+
+  chargePhasers() {
+    if (!this.shipId) {
+      return 'had no effect'
+    }
+  }
+  engageWarpDrive() {
+    if (!this.shipId) {
+      return 'had no effect'
+    }
+  }
+  setsInvisibility() {
+    if (!this.currentShip) {
+      return 'had no effect'
+    }else if(this.position == "Defender"){
+      this.currentShip.cloaked = true
+    }
+  }
+
+
+
+  assignShip(ship) {
+    this.currentShip = ship
+  }
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,27 @@
-class Spaceship {}
+let spaceships = []
+
+let spaceshipId = 0
+class Spaceship {
+  constructor(name, crew, phasers, shields, cloaked = false, warpDrive='disengaged') {
+
+    this.name = name
+    this.crew = crew
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = cloaked
+    this.warpDrive = warpDrive
+    this.phasersCharge = 'uncharged'
+    this.id = ++spaceshipId
+
+    if (crew.length > 0) {
+      this.docked = false
+    } else {
+      this.docked = true
+    }
+    spaceships.push(this)
+
+    this.crew.forEach(function(member) {member.currentShip = this}.bind(this))
+    // !crew == [] ? this.docked = true : this.docked = false
+
+  }
+}
